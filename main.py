28314f6e1185e9a0a1eef771dda57f882cfae02e
spgitmonish <monish.sunku@walletbuddies.co>
2017-08-28 20:43:29 -0600

    First test

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import warnings
 from distutils.version import LooseVersion
 import project_tests as tests
 
-
 # Check TensorFlow Version
 assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
 print('TensorFlow Version: {}'.format(tf.__version__))
@@ -16,7 +15,6 @@ if not tf.test.gpu_device_name():
 else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
-
 def load_vgg(sess, vgg_path):
     """
     Load Pretrained VGG Model into TensorFlow.
@@ -24,18 +22,38 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
+    # Tag for loading the downloaded and saved VGG-16 ConvNet
     vgg_tag = 'vgg16'
+
+    # Load the model and weights(using the session and tag)
+    # from the given path(vgg_path)
+    vgg_model = tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    vgg_model.summary()
+
+    # These tensors names are used for creating a FCN(Fully Convolutional Network)
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
-tests.test_load_vgg(load_vgg, tf)
 
+    # Use the default graph for this thread(should be related to the model loaded)
+    vgg_graph = tf.get_default_graph()
+
+    # Input tensor
+    vgg_input_tensor = vgg.get_tensor_by_name(vgg_input_tensor_name)
+    # Dropout layer tensor
+    vgg_keep_prob_tensor = vgg.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    # Output of pool layer 3
+    vgg_layer3_out_tensor = vgg.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    # Output of pool layer 4
+    vgg_layer4_out_tensor = vgg.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    # Output of layer 7
+    vgg_layer7_out_tensor = vgg.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    # Return a tuple of tensors from this function
+    return vgg_input_tensor, vgg_keep_prob_tensor, vgg_layer3_out_tensor, vgg_layer4_out_tensor, vgg_layer7_out_tensor
+tests.test_load_vgg(load_vgg, tf)
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
@@ -46,10 +64,30 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
-tests.test_layers(layers)
+    # Build FCN-8 decoder using upsampling and adding skip connections
+    # First make sure that the output shape is same(apply 1x1 convolution)
+    vgg_layer7_logits = tf.layers.conv2d(vgg_layer7_out, num_classes, kernel_size=1, name='vgg_layer7_logits')
+    vgg_layer4_logits = tf.layers.conv2d(vgg_layer4_out, num_classes, kernel_size=1, name='vgg_layer7_logits')
+    vgg_layer3_logits = tf.layers.conv2d(vgg_layer3_out, num_classes, kernel_size=1, name='vgg_layer7_logits')
 
+    # Upsample the output of 1x1 convolution output(start of the decoding process)
+    fcn_1 = tf.layers.conv2d_transpose(vgg_layer7_logits, num_classes, kernel_size=4, strides=(2, 2), padding='same', name='fcn_1')
+
+    # Add skip connection from the output of pool layer 4 of VGG
+    fcn_2 = tf.add(fcn_1, vgg_layer4_logits, name='fcn_2')
+
+    # Upsample output from fcn_2
+    fcn_3 = tf.layers.conv2d_transpose(fcn_2, num_classes, kernel_size=4, strides=(2, 2), padding='same', name='fcn_3')
+
+    # Add skip connection from the output of pool layer 3 of VGG
+    fcn_4 = tf.add(fcn_1, vgg_layer4_logits, name='fcn_2')
+
+    # Final decoder output after last upsampling
+    fcn_output = tf.layers.conv2d_transpose(fcn_decoder_layer4, num_classes, kernel_size=16, strides=(8, 8), padding='same', name='fcn_output')
+
+    # Return the output tensor(decoder output)
+    return fcn_output
+tests.test_layers(layers)
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
@@ -60,12 +98,23 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+    # Reshape the 4D output and label tensors to 2D:
+    # Each row represents a pixel and each column a class.
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    y = tf.reshape(correct_label, (-1, num_classes))
+
+    # Define a loss function(softmax with logits and cross entropy)
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+
+    # Use an optimizer for reducing loss(Using Adam optimizer)
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+
+    # Return the result
+    return logits, optimizer, cross_entropy_loss
 tests.test_optimize(optimize)
 
-
-def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+def train_nn(sess, epochs, batch_size, get_batches_fn, train_op,
+             cross_entropy_loss, input_image,
              correct_label, keep_prob, learning_rate):
     """
     Train neural network and print out the loss during training.
@@ -80,10 +129,58 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
+    # Initialize all the global variables
+    sess.run(tf.global_variables_initializer())
+
+    # Run for a certain number of epochs
+    for epoch in epochs:
+        print("Epoch: ", epoch)
+
+        # Train on batches(1 image)
+        training_loss = 0
+        training_samples = 0
+        for X, y in get_batches_fn(batch_size):
+            training_samples += len(X)
+            loss, _ = sess.run([cross_entropy_loss, train_op], feed_dict={input_image: X, correct_label: y, keep_prob: 0.8})
+            training_loss += loss
+
+        # Calculate training loss
+        training_loss /= training_samples
+        print("Training loss: {}".format(training_loss))
 tests.test_train_nn(train_nn)
 
+def save_inference_samples(runs_dir, data_dir, sess, image_shape,
+                           logits, keep_prob, input_image, epoch):
+    """
+    save model weights and generate samples.
+    :param runs_dir: directory where model weights and samples will be saved
+    :param data_dir: directory where the Kitty dataset is stored
+    :param sess: TF Session
+    :param image_shape: shape of the input image for prediction
+    :param logits: TF Placeholder for the FCN prediction
+    :param keep_prob: TF Placeholder for dropout keep probability
+    :param input_image: TF Placeholder for input images
+    :param epochs: Number of epochs or Final label
+    """
+    # Make folder for current run
+    output_dir = os.path.join(runs_dir, str(time.time()))
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
+    # Run NN on test images and save them to disk
+    print('Epoch {} finished. Saving test images to: {}'.format(epoch, output_dir))
+    image_outputs = helper.gen_test_output(sess, logits, keep_prob, input_image, os.path.join(data_dir, 'data_road/testing'), image_shape)
+
+    # Save the image output
+    for name, image in image_outputs:
+        scipy.misc.imsave(os.path.join(output_dir, name), image)
+
+    # Save the model
+    saver = tf.train.Saver()
+    filefcn_path = os.path.join(output_dir, 'fcn-{}.ckpt'.format(epoch))
+    save_path = saver.save(sess, filefcn_path)
+    print('Model saved to: {}'.format(filefcn_path))
 
 def run():
     num_classes = 2
@@ -99,24 +196,47 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
+    # Hyperparameters for training
+    epochs = 25
+    batch_size = 1
+    lr = 0.0001
+    learning_rate = tf.constant(lr)
+
+    # Download the VGG-16 model if it doesn't exist
+    helper.maybe_download_pretrained_vgg(data_dir)
+
     with tf.Session() as sess:
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
+
         # Create function to get batches
         get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
 
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
-        # TODO: Build NN using load_vgg, layers, and optimize function
+        # Build NN using load_vgg, layers, and optimize function
+        # Placeholder for model training(batch size, shape[0], shape[1], num_classes)
+        correct_label = tf.placeholder(tf.float32, [None, image_shape[0], image_shape[1], num_classes])
 
-        # TODO: Train NN using the train_nn function
+        # Get the VGG-16 layers
+        vgg_input, keep_prob, vgg_layer3, vgg_layer4, vgg_layer7 = load_vgg(sess, vgg_path)
 
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        # Get the last layer(output) of the network
+        fcn_output = layers(vgg_layer3, vgg_layer4, vgg_layer7, num_classes)
+
+        # Get the logits, optimizer and cross entropy loss
+        logits, optimizer, cross_entropy_loss = optimize(fcn_output, correct_label, learning_rate, num_classes)
+
+        # Train NN using the train_nn function
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op,
+                 cross_entropy_loss, vgg_input, correct_label, keep_prob,
+                 lr, runs_dir, data_dir, image_shape, logits)
+
+        # Save the inference data from the run
+        save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, vgg_input, 'FINAL')
 
         # OPTIONAL: Apply the trained model to a video
-
 
 if __name__ == '__main__':
     run()
