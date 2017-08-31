e612112805d6e125df6e2ed8e1ac4f60c93b55bc
spgitmonish <monish.sunku@walletbuddies.co>
2017-08-30 21:12:28 -0600

    Add histogram equalization and modify comments

--- helper.py
+++ helper.py
@@ -100,8 +100,14 @@ def gen_batch_function(data_folder, image_shape):
                 y_translate = np.random.uniform(-40, 40)
                 translate_matrix = np.float32([[1, 0, x_translate],[0, 1, y_translate]])
                 image = cv2.warpAffine(image, translate_matrix, (image_shape[1], image_shape[0]))
-		# NOTE: Both the images(original and ground truth) have the same shape
+		        # NOTE: Both the images(original and ground truth) have the same shape
                 gt_image = cv2.warpAffine(gt_image, translate_matrix, (image_shape[1], image_shape[0]))
+
+                # Add histogram equalization along the 'Y' channel of a YUV image
+                # NOTE: This is supposed to help remove shadows in the image
+                image_yuv = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+                image_yuv[:,:,0] = cv2.equalizeHist(image_yuv[:,:,0])
+                image = cv2.cvtColor(image_yuv, cv2.COLOR_YUV2RGB)
 
                 # Add random luminance changes
                 # NOTE: Only the original image is converted from RGB->HLS and back
@@ -109,10 +115,12 @@ def gen_batch_function(data_folder, image_shape):
                 image[:,:,1] = image[:,:,1] * (0.25 + np.random.uniform(0.25, 0.75))
                 image = cv2.cvtColor(image, cv2.COLOR_HLS2RGB)
 
+                # Array representing the ground truth in boolean form
                 gt_bg = np.all(gt_image == background_color, axis=2)
                 gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
                 gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
 
+                # Append the original images and the ground truth labels
                 images.append(image)
                 gt_images.append(gt_image)
 
@@ -131,11 +139,11 @@ def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape)
     :return: Output for for each test image
     """
     for image_file in glob(os.path.join(data_folder, 'image_2', '*.png')):
+        # Resize the image to the input image size
         image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
 
-        im_softmax = sess.run(
-            [tf.nn.softmax(logits)],
-            {keep_prob: 1.0, image_pl: [image]})
+        # Perform segmentation to the test image(add a mask reresenting predictions)
+        im_softmax = sess.run([tf.nn.softmax(logits)],{keep_prob: 1.0, image_pl: [image]})
         im_softmax = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
         segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
         mask = np.dot(segmentation, np.array([[0, 255, 0, 127]]))
@@ -143,6 +151,7 @@ def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape)
         street_im = scipy.misc.toimage(image)
         street_im.paste(mask, box=None, mask=mask)
 
+        # Add the prediction to the folder
         yield os.path.basename(image_file), np.array(street_im)
 
 def save_inference_samples(runs_dir, data_dir, sess, image_shape,
