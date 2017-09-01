ade5df738ecd295158023956d80899f61fdaf12d
spgitmonish <monish.sunku@walletbuddies.co>
2017-08-31 22:23:56 -0600

    Go back to epochs=50, b=5

--- main.py
+++ main.py
@@ -190,8 +190,8 @@ def run():
     #  https://www.cityscapes-dataset.com/
 
     # Hyperparameters for training
-    epochs = 100
-    batch_size = 10
+    epochs = 50
+    batch_size = 5
     lr = 0.0001
     learning_rate = tf.constant(lr)
 
